77aa05e706e5097a108577bd4a78f2dbeb1e443e
Batuhanirmali <batuqwert1@gmail.com>
2025-12-08 12:29:27 +0300

    Initial Commit

---
+++ Nexoft-TestCase/Nexoft_TestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nexoft_TestCaseApp.swift
+//  Nexoft-TestCase
+//
+//  Created by Talha Batuhan Irmalı on 8.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nexoft_TestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
